b028e513d1736dd24852bc52c02a1d1435799066
chae-hwa <ihj0519@naver.com>
2022-07-29 01:30:53 +0900

    json 응답에는 무조건 resultCode와 msg가 있어야돼서, ResultData 클래스 생성

---
+++ src/main/java/com/ll/exam/ResultData.java
@@ -0,0 +1,16 @@
+package com.ll.exam;
+
+import com.ll.exam.article.dto.ArticleDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class ResultData<T> {
+    private String msg;
+    private String resultCode;
+    private T data;
+
+}
