36b445e260ee9fb2c129892ebacfa6874f97fa94
chae-hwa <ihj0519@naver.com>
2022-07-28 14:50:23 +0900

    잭슨으로 객체를 json으로 변경

---
+++ src/main/java/com/ll/exam/util/Ut.java
@@ -0,0 +1,18 @@
+package com.ll.exam.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Ut {
+    public static class json {
+        public static String toJsonStr(Object obj, String defaultValue) {
+            ObjectMapper om = new ObjectMapper();
+
+            try {
+                return om.writeValueAsString(obj); // 자바 객체를 json으로 (문자열로)변환
+            } catch (JsonProcessingException e) {
+                return defaultValue;
+            }
+        }
+    };
+}
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,11 +1,28 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.article.dto.ArticleDto;
+import com.ll.exam.util.Ut;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AppTest {
     @Test
-    void 테스트_assertThat(){
+    void 실험_assertThat(){
         int rs = 10 + 20;
         assertThat(rs).isEqualTo(30);
+    }
+
+    @Test
+    void 실험_ObjectMapper() throws JsonProcessingException {
+        ArticleDto articleDto = new ArticleDto(1,"제목","내용");
+
+        String jsonStr = Ut.json.toJsonStr(articleDto, "");// articleDto을 줄 테니까 json으로 바꿔와봐
+                                                                     // 실패하면 빈문자열로 갖고 오고
+        assertThat(jsonStr).isNotBlank(); //jsonStr은 빈문자열이 아니다.
+        assertThat(jsonStr).isEqualTo("""
+                {"id":1,"title":"제목","body":"내용"}
+                """.trim()); // 자바 객체가 json으로 변환된 게 jsonStr이면
+                             // jsonStr이 저 출력 형식과 동일해야 된다.
     }
 }
